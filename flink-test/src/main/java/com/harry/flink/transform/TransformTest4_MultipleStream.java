21cc56aeb433539351ae6b662d1d0d3923cce3e3
haofan6786 <46396551+haofan6786@users.noreply.github.com>
2021-09-14 23:22:00 +0800

    update

--- flink-test/src/main/java/com/harry/flink/transform/TransformTest4_MultipleStream.java
+++ flink-test/src/main/java/com/harry/flink/transform/TransformTest4_MultipleStream.java
@@ -1,10 +1,16 @@
 package com.harry.flink.transform;
 
 import com.harry.flink.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 
 import java.util.Collections;
 
@@ -29,7 +35,29 @@ public class TransformTest4_MultipleStream {
         high.print("high");
         low.print("low");
         all.print("all");
-        env.execute();
 
+        SingleOutputStreamOperator<Tuple2<String, Double>> warningStream = high.map(new MapFunction<SensorReading, Tuple2<String, Double>>() {
+            @Override
+            public Tuple2<String, Double> map(SensorReading sensorReading) throws Exception {
+                return new Tuple2<>(sensorReading.getId(),sensorReading.getTemperature());
+            }
+        });
+        ConnectedStreams<Tuple2<String, Double>, SensorReading> connectedStreams = warningStream.connect(low);
+
+        SingleOutputStreamOperator<Object> result = connectedStreams.map(new CoMapFunction<Tuple2<String, Double>, SensorReading, Object>() {
+            @Override
+            public Object map1(Tuple2<String, Double> value) throws Exception {
+                return new Tuple3<>(value.f0, value.f1, "high temp warning");
+            }
+
+            @Override
+            public Object map2(SensorReading value) throws Exception {
+                return new Tuple2<>(value.getId(), "normal");
+            }
+        });
+
+        result.print();
+
+        env.execute();
     }
 }
