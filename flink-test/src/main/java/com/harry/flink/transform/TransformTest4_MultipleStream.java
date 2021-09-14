484572ade1c620b9730620cac5fe8dcdfed443a1
haofan6786 <46396551+haofan6786@users.noreply.github.com>
2021-09-14 22:25:57 +0800

    update

--- flink-test/src/main/java/com/harry/flink/transform/TransformTest4_MultipleStream.java
+++ flink-test/src/main/java/com/harry/flink/transform/TransformTest4_MultipleStream.java
@@ -5,14 +5,21 @@ import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.util.Collections;
+
 public class TransformTest4_MultipleStream {
     public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         DataStream<String> inputStream = env.readTextFile("C:\\Users\\Harry\\IdeaProjects\\scala_tutor\\src\\main\\resources\\sensor.txt");
-        DataStream<SensorReading> dataStream = inputStream.map(line->{
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
 
-        })
+        dataStream.split((OutputSelector<SensorReading>) sensorReading -> {
+            return (sensorReading.getTemperature() > 30) ? Collections.singletonList("high") : Collections.singletonList("low");
+        });
     }
 }
