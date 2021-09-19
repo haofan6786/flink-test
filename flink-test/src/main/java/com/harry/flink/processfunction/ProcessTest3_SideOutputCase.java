5d7e2954a2ac5a9267e22082d9e50abb5b28b90b
haofan6786 <46396551+haofan6786@users.noreply.github.com>
2021-09-19 14:25:12 +0800

    update

--- flink-test/src/main/java/com/harry/flink/processfunction/ProcessTest3_SideOutputCase.java
+++ flink-test/src/main/java/com/harry/flink/processfunction/ProcessTest3_SideOutputCase.java
@@ -5,6 +5,9 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 public class ProcessTest3_SideOutputCase {
     public static void main(String[] args) throws Exception {
@@ -16,12 +19,29 @@ public class ProcessTest3_SideOutputCase {
             return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
         });
 
+        //定义OutputTag
+        OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("lowTemp") {
+        };
+
         //自定义侧输出流实现分流操作
-        dataStream
-                .keyBy("id")
-                .process(new ProcessTest2_ApplicationCase.TempConsIncreWarning(
-                        10));
-        resultStream.print();
+        SingleOutputStreamOperator<SensorReading> highTempStream = dataStream
+                .process(new ProcessFunction<SensorReading, SensorReading>() {
+                    @Override
+                    public void processElement(SensorReading value, Context ctx, Collector<SensorReading> out) throws Exception {
+                        //判断温度大于30，输出到主流，小的话就输出到侧输出流
+                        if (value.getTemperature()>30){
+                            out.collect(value);
+                        }
+                        else {
+                            ctx.output(outputTag,value);
+                        }
+                    }
+                });
+
+        highTempStream.print("high-temp");
+        highTempStream.getSideOutput(outputTag).print("low-temp");
+
+
         env.execute();
     }
 }
