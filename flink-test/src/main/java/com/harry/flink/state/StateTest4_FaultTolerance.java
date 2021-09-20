e849784e0a94ca42bdf8d4b16398542922a555ca
haofan6786 <46396551+haofan6786@users.noreply.github.com>
2021-09-20 13:54:07 +0800

    update

--- flink-test/src/main/java/com/harry/flink/state/StateTest4_FaultTolerance.java
+++ flink-test/src/main/java/com/harry/flink/state/StateTest4_FaultTolerance.java
@@ -1,9 +1,12 @@
 package com.harry.flink.state;
 
 import com.harry.flink.beans.SensorReading;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -17,6 +20,23 @@ public class StateTest4_FaultTolerance {
         env.setStateBackend(new MemoryStateBackend());
         env.setStateBackend(new FsStateBackend(""));
         env.setStateBackend(new RocksDBStateBackend(""));
+
+
+        //2.检查点配置
+        env.enableCheckpointing(300);
+
+        //高级选项
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60000L);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(100L);
+        env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+        env.getCheckpointConfig().setTolerableCheckpointFailureNumber(0);
+
+        //重启策略配置
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,10000));//固定延时重启
+        env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.minutes(10),Time.minutes(1)));//失败率重启
+
 
         DataStreamSource<String> inputStream = env.socketTextStream("localhost", 7777);
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
