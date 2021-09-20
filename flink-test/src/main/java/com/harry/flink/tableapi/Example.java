4f4d8ad65236e55d8f492c6fbffdb4c6f06d2e93
haofan6786 <46396551+haofan6786@users.noreply.github.com>
2021-09-20 16:37:53 +0800

    update

---
+++ flink-test/src/main/java/com/harry/flink/tableapi/Example.java
@@ -0,0 +1,44 @@
+package com.harry.flink.tableapi;
+
+import com.harry.flink.beans.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+public class Example {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<String> inputStream = env.readTextFile("/Users/harry/IdeaProjects/flink-test/flink-test/src/main/resources/sensor.txt");
+
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0],
+                    new Long(fields[1]),
+                    new Double(fields[2]));
+        });
+
+        //创建表环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        //基于流创建一张表
+        Table dataTable = tableEnv.fromDataStream(dataStream);
+
+        //调用table API进行转换操作
+        Table resultTable = dataTable.select("id,temperature").where("id='sensor_1'");
+
+        //执行SQL
+        tableEnv.createTemporaryView("sensor",dataTable);
+        String sql = "select id,temperature from sensor where id = 'sensor_1'";
+        Table resultSqlTable = tableEnv.sqlQuery(sql);
+
+        tableEnv.toAppendStream(resultTable, Row.class).print("result");
+        tableEnv.toAppendStream(resultSqlTable,Row.class).print("sql");
+
+        env.execute();
+    }
+}
